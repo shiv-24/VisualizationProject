37db08d73a03988d3edf7ee56f1943d447694a3b
shiv-24 <shivaggarwal.shivam1107@gmail.com>
2018-11-11 11:42:31 -0500

    Added New Project

---
+++ VisualizationUsingJava8/src/main/java/java8/visualization/VisualizationUsingJava8/App.java
@@ -0,0 +1,13 @@
+package java8.visualization.VisualizationUsingJava8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
